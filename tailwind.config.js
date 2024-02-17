d080ba5a502de94ef1530c6ee68b8fb05519ec4e
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-02-17 10:15:52 -0700

    commit initial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      fontFamily: {
+        'inter': ["Inter", "sans-serif"]
+      }
+    },
+    colors: {
+      'black-cust': '#141414',
+      'dark-grey': '#1F1F1F',
+      'grey-cust': '#333333',
+      'neon-green': '#C4F82A',
+      'white': '#ffffff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
